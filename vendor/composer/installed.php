5f44a1cc5d8915f6b607ca667b07ea0306b08384
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-23 09:55:46 +0100

    Version v0.1.0-dev

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'icinga/abandoned',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dde31ed8356de1deaaf2008f636d6551b820246f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'icinga/abandoned' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dde31ed8356de1deaaf2008f636d6551b820246f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
