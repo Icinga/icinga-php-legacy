febf549b7690bf21b8dc691678c55517a4342768
Eric Lippmann <eric.lippmann@icinga.com>
2026-03-27 12:29:42 +0100

    Release version 1.0.1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'icinga/icinga-php-legacy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e4a6b04a4f687ff0583d3a135cbb39fe5b3d6359',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'icinga/icinga-php-legacy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e4a6b04a4f687ff0583d3a135cbb39fe5b3d6359',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
